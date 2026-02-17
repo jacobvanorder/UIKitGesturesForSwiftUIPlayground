622cf371b28b39cc79afe6d07b2fe8faa2b0d114
Jacob Van Order <jacob@sushigrass.com>
2026-02-17 16:33:23 -0600

    Initial Commit.

---
+++ UIKitGesturesForSwiftUIPlayground/UIKitGesturesForSwiftUIPlayground/UIKitGesturesForSwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitGesturesForSwiftUIPlaygroundApp.swift
+//  UIKitGesturesForSwiftUIPlayground
+//
+//  Created by Jacob Van Order on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct UIKitGesturesForSwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UIKitGesturesForSwiftUIPlayground/UIKitGesturesForSwiftUIPlaygroundTests/UIKitGesturesForSwiftUIPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitGesturesForSwiftUIPlaygroundTests.swift
+//  UIKitGesturesForSwiftUIPlaygroundTests
+//
+//  Created by Jacob Van Order on 2/17/26.
+//
+
+import Testing
+@testable import UIKitGesturesForSwiftUIPlayground
+
+struct UIKitGesturesForSwiftUIPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
